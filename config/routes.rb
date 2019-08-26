886e79aa00e3ce511dd659914cd8f67f9141a4d8
santiestra <santiago.estrago@rootstrap.com>
2019-08-26 20:56:47 -0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
+  get 'users/update'
+  get 'users/edit'
+  get 'users/destroy'
+  get 'users/index'
+  get 'users/show'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
