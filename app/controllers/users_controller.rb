afa7975ad4b0ba1adc0c4699d7dea15dba93220f
santiestra <santiago.estrago@rootstrap.com>
2019-08-26 22:52:20 -0300

    Add create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
   end
 
   def update
@@ -15,8 +22,15 @@ class UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
   end
 
   def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/update'
-  get 'users/edit'
-  get 'users/destroy'
-  get 'users/index'
-  get 'users/show'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
