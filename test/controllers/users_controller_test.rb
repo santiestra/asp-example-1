0f87121e6559b5a5633153cb2bc5648577e6878e
santiestra <santiago.estrago@rootstrap.com>
2019-09-01 17:06:29 -0300

    Fix tests for users controller, change gitignore

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,38 +2,22 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get users_create_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get users_update_url
+    get new_user_path
     assert_response :success
   end
 
   test "should get edit" do
-    get users_edit_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get users_destroy_url
+    get edit_user_path(User.first)
     assert_response :success
   end
 
   test "should get index" do
-    get users_index_url
+    get users_path
     assert_response :success
   end
 
   test "should get show" do
-    get users_show_url
+    get user_path(User.first)
     assert_response :success
   end
-
 end
