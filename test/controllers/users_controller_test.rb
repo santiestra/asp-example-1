e86fcf539b61af353d313a1a29e2155dc76743c2
santiestra <santiago.estrago@rootstrap.com>
2019-09-22 21:24:49 -0300

    Add rubocop, still need some fixes

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  test 'should get new' do
     get new_user_path
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_user_path(User.first)
     assert_response :success
   end
 
-  test "should get index" do
+  test 'should get index' do
     get users_path
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get user_path(User.first)
     assert_response :success
   end
